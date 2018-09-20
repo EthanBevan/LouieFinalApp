11f40e33bef18d66313f6553f200a67c2c7b11dd
Ethan <ethanbevan00@yahoo.com>
2018-09-20 20:27:49 +1000

    4th

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "plexusMusic_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "LouieFinalAppRepository_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
@@ -89,3 +89,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+config.serve_static_assets = true
+config.assets.compile = true
